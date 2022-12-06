0237801d7cc4edc5b424478825e43b3b15833fc5
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-12-06 15:08:40 -0500

    Modificando y agregando algunos comentarios sobre las propiedades en JavaScript y de cómo manipular el DOM(Document Object Model).

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ console.log({
 });
 
 /* Como consulta, podemos acceder a las etiquetas, ids o clases como se ve aquí. */
-console.log(document.getElementsByTagName('li')); // Accede al elemento por medio de la etiqueta 'li'.
+
+/* console.log(document.getElementsByTagName('li')); // Accede al elemento por medio de la etiqueta 'li'.
 console.log(document.getElementsByClassName('card')); // Accede al elemento por medio de la clase 'card'.
 console.log(document.getElementsByName('nombre')); // Accede a los elementos por medio del atributo 'name' del documento, name se utiliza en los formularios.
 console.log(document.getElementById('menu')); // Accede a los elementos por medio del identificador 'menu'.
@@ -37,4 +38,23 @@ console.log(document.querySelectorAll('a')); // Accede al elemento por medio de 
 console.log(document.querySelector('.card')); // Accede al elemento por medio de la clase '.card'.
 console.log(document.querySelector('#card')); // Accede a los elementos por medio del identificador '#card'.
 console.log(document.querySelectorAll('.card')[2]); // Accede al elemento por medio de la clase '.card' y busca al elemento en la posición indicada.
-console.log(document.querySelectorAll('.menu li')); // Accede a todos los elementos 'li' que tengan la clase 'menu'.
+console.log(document.querySelectorAll('.menu li')); // Accede a todos los elementos 'li' que tengan la clase 'menu'. */
+
+h1.innerHTML = 'Patito <br> Feo'; // Aquí modificamos con sintaxis HTML, no es seguro.
+h1.innerText = 'Patito <br> Feo'; // Aquí no modificamos con sintaxis HTML, es más seguro.
+console.log(h1.getAttribute('class')); // Trae el valor del atributo que se le ingrese.
+// h1.setAttribute('class', 'rojo'); // Cambia el valor del atributo 'class' a 'rojo'.
+h1.classList.add('rojo'); // Agrega la clase 'rojo' a la variable h1.
+h1.classList.remove('verde'); // Quita el valor 'verde' de la clase en h1.
+
+
+input.value = "456"; // Valor por defecto del input.
+
+console.log(document.createElement('img'));
+console.log(document.createElement('span'));
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&w=400');
+
+pid.innerHTML = "";
+pid.appendChild(img);
