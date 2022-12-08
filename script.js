0e9083de7a14258d1f29d9306b5f83020bdecac3
Aaron Ilizarbe <aaron.d.ilizarbe.s@gmail.com>
2022-12-08 17:59:22 -0500

    addEventListener para escuchar las acciones del usuario con la interfaz.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-function btnOnClick() {
+form.addEventListener('submit', sumarInputValues);
+/* El último botón de un formulario es por defecto tipo submit
+    a menos que especifiquemos lo contrario. */
+function sumarInputValues(event) {
+    console.log({event});
+    event.preventDefault(); // Agregamos esto para que no se refresque la página.
     const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
     pResult.innerText = "Resultado: " + sumaInputs;
 }
